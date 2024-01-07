b4a7f94cfe45816ea884033f4473506103378fb2
AlexSkilled <olegthander@yandex.ru>
2024-01-07 16:38:10 +0300

    [master] moved api

--- grpc.mk
+++ grpc.mk
@@ -23,7 +23,7 @@ gen-server: .pre-gen-server .gen-server
         	--grpc-gateway_out=logtostderr=true:./pkg/api/ \
         	--swagger_out=allow_merge=true,merge_file_name=api:./api \
     		--descriptor_set_out=./pkg/api/api_discriptor.pb \
-        	--go_out=./pkg/api/. \
-        	--go-grpc_out=./pkg/api/. \
+        	--go_out=./pkg/. \
+        	--go-grpc_out=./pkg/. \
         	./api/grpc/*.proto
 
