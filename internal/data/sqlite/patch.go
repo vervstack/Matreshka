fb93d05387093c131795b2064cc89103c7cc7628
AlexSkilled <olegthander@yandex.ru>
2024-07-22 11:24:51 +0300

    [master] perun-linked-changes

--- internal/data/sqlite/patch.go
+++ internal/data/sqlite/patch.go
@@ -9,13 +9,26 @@ import (
 )
 
 func (p *Provider) PatchConfig(ctx context.Context, req domain.PatchConfigRequest) error {
+	var configId int
+	err := p.conn.QueryRowContext(ctx, `
+			SELECT 
+				id
+			FROM configs 
+			WHERE name = $1`,
+		req.ServiceName).
+		Scan(&configId)
+	if err != nil {
+		return errors.Wrap(err, "error getting service config id")
+	}
+
 	for _, b := range req.Batch {
-		_, err := p.conn.ExecContext(ctx,
-			`
-		INSERT INTO configs_values 
-			    (key, value)
-		VALUES 	( $1,    $2)`,
-			b.FieldName, b.FieldValue)
+		_, err := p.conn.ExecContext(ctx, `
+			INSERT INTO configs_values 
+					(config_id, key, value)
+			VALUES 	(       $1,  $2,    $3) 
+			ON CONFLICT (config_id, key) 
+			DO UPDATE SET value = excluded.value`,
+			configId, b.FieldName, b.FieldValue)
 		if err != nil {
 			return errors.Wrap(err, "error upserting config")
 		}
