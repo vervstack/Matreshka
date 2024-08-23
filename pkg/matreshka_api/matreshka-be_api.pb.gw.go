2aaf835564bdf4093c7d2fc1019f62cc4aa8b4b9
AlexSkilled <olegthander@yandex.ru>
2024-08-23 21:59:49 +0400

    "added http"

--- pkg/matreshka_api/matreshka-be_api.pb.gw.go
+++ pkg/matreshka_api/matreshka-be_api.pb.gw.go
@@ -303,6 +303,7 @@ func local_request_MatreshkaBeAPI_PatchConfig_0(ctx context.Context, marshaler r
 // UnaryRPC     :call MatreshkaBeAPIServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
 // Note that using this registration option will cause many gRPC library features to stop working. Consider using RegisterMatreshkaBeAPIHandlerFromEndpoint instead.
+// GRPC interceptors will not work for this type of registration. To use interceptors, you must use the "runtime.WithMiddlewares" option in the "runtime.NewServeMux" call.
 func RegisterMatreshkaBeAPIHandlerServer(ctx context.Context, mux *runtime.ServeMux, server MatreshkaBeAPIServer) error {
 
 	mux.Handle("GET", pattern_MatreshkaBeAPI_ApiVersion_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
@@ -493,7 +494,7 @@ func RegisterMatreshkaBeAPIHandler(ctx context.Context, mux *runtime.ServeMux, c
 // to "mux". The handlers forward requests to the grpc endpoint over the given implementation of "MatreshkaBeAPIClient".
 // Note: the gRPC framework executes interceptors within the gRPC handler. If the passed in "MatreshkaBeAPIClient"
 // doesn't go through the normal gRPC flow (creating a gRPC client etc.) then it will be up to the passed in
-// "MatreshkaBeAPIClient" to call the correct interceptors.
+// "MatreshkaBeAPIClient" to call the correct interceptors. This client ignores the HTTP middlewares.
 func RegisterMatreshkaBeAPIHandlerClient(ctx context.Context, mux *runtime.ServeMux, client MatreshkaBeAPIClient) error {
 
 	mux.Handle("GET", pattern_MatreshkaBeAPI_ApiVersion_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
