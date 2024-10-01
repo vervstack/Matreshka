9793fbc57d5314aa427849ceaca49bb136fe0492
AlexSkilled <olegthander@yandex.ru>
2024-10-01 17:24:52 +0300

    "rscli auto-commit"

--- internal/clients/sqldb/conn.go
+++ internal/clients/sqldb/conn.go
@@ -1,7 +1,10 @@
 package sqldb
 
 import (
+	"context"
 	"database/sql"
+	"database/sql/driver"
+	"time"
 
 	"github.com/Red-Sock/toolbox/closer"
 	"github.com/Red-Sock/trace-errors"
@@ -11,7 +14,7 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-func New(cfg resources.SqlResource) (*sql.DB, error) {
+func New(cfg resources.SqlResource) (DB, error) {
 	dialect := cfg.SqlDialect()
 	connStr := cfg.ConnectionString()
 
@@ -32,4 +35,27 @@ func New(cfg resources.SqlResource) (*sql.DB, error) {
 	}
 
 	return conn, nil
+}
+
+type DB interface {
+	PingContext(ctx context.Context) error
+	Ping() error
+	Close() error
+	SetMaxIdleConns(n int)
+	SetMaxOpenConns(n int)
+	SetConnMaxLifetime(d time.Duration)
+	SetConnMaxIdleTime(d time.Duration)
+	Stats() sql.DBStats
+	PrepareContext(ctx context.Context, query string) (*sql.Stmt, error)
+	Prepare(query string) (*sql.Stmt, error)
+	ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error)
+	Exec(query string, args ...any) (sql.Result, error)
+	QueryContext(ctx context.Context, query string, args ...any) (*sql.Rows, error)
+	Query(query string, args ...any) (*sql.Rows, error)
+	QueryRowContext(ctx context.Context, query string, args ...any) *sql.Row
+	QueryRow(query string, args ...any) *sql.Row
+	BeginTx(ctx context.Context, opts *sql.TxOptions) (*sql.Tx, error)
+	Begin() (*sql.Tx, error)
+	Driver() driver.Driver
+	Conn(ctx context.Context) (*sql.Conn, error)
 }
