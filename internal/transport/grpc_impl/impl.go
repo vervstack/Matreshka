6f9b1a9a4358ebfe6f992fa927f988b8b9636a1a
AlexSkilled <olegthander@yandex.ru>
2024-11-20 11:36:28 +0300

    [master] moved stuff to impl.go

--- internal/transport/grpc_impl/impl.go
+++ internal/transport/grpc_impl/impl.go
@@ -1,6 +1,14 @@
 package grpc_impl
 
 import (
+	"context"
+	"net/http"
+
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+
+	"github.com/godverv/matreshka-be/internal/config"
 	"github.com/godverv/matreshka-be/internal/service"
 	"github.com/godverv/matreshka-be/internal/storage"
 	"github.com/godverv/matreshka-be/pkg/matreshka_be_api"
@@ -12,4 +20,36 @@ type Impl struct {
 	storage storage.Data
 
 	matreshka_be_api.UnimplementedMatreshkaBeAPIServer
+}
+
+func NewServer(
+	cfg config.Config,
+	service service.ConfigService,
+	storage storage.Data,
+) *Impl {
+	return &Impl{
+		version: cfg.AppInfo.Version,
+		service: service,
+		storage: storage,
+	}
+}
+
+func (a *Impl) Register(srv *grpc.Server) {
+	matreshka_be_api.RegisterMatreshkaBeAPIServer(srv, a)
+}
+
+func (a *Impl) Gateway(ctx context.Context, endpoint string, opts ...grpc.DialOption) (route string, handler http.Handler) {
+	gwHttpMux := runtime.NewServeMux()
+
+	err := matreshka_be_api.RegisterMatreshkaBeAPIHandlerFromEndpoint(
+		ctx,
+		gwHttpMux,
+		endpoint,
+		opts,
+	)
+	if err != nil {
+		logrus.Errorf("error registering grpc2http handler: %s", err)
+	}
+
+	return "/api/", gwHttpMux
 }
