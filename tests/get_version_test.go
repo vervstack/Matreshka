bfea2b9d4fab35017b06b33121b750c083d7ddbf
AlexSkilled <olegthander@yandex.ru>
2025-02-15 12:33:58 +0300

    [master] removed extra tests

--- tests/get_version_test.go
+++ tests/get_version_test.go
@@ -1,11 +1,7 @@
-//go:build integration
-
 package tests
 
 import (
 	"context"
-	"io"
-	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
@@ -19,20 +15,10 @@ type GetVersionSuite struct {
 
 func (s *GetVersionSuite) Test_GetVersion() {
 	ctx := context.Background()
-	resp, err := testEnv.grpcImpl.ApiVersion(ctx, &matreshka_be_api.ApiVersion_Request{})
+	resp, err := testEnv.matreshkaApi.ApiVersion(ctx, &matreshka_be_api.ApiVersion_Request{})
 
 	s.Require().NoError(err)
 	s.Require().NotNil(resp)
-}
-
-func (s *GetVersionSuite) Test_Gateway() {
-	resp, err := http.Get("http://localhost:8080/api/version")
-	s.Require().NoError(err)
-
-	bd, err := io.ReadAll(resp.Body)
-	s.Require().NoError(err)
-
-	s.Require().Equal(`{"version":"v1.0.32"}`, string(bd))
 }
 
 func Test_GetVersion(t *testing.T) {
