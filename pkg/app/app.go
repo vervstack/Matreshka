ebaf8a254757aa29a418ebd2bbafc59b26c5a451
AlexSkilled <olegthander@yandex.ru>
2025-03-22 08:31:41 +0300

    [master] added app pkg

---
+++ pkg/app/app.go
@@ -0,0 +1,16 @@
+package app
+
+import (
+	"go.vervstack.ru/matreshka-be/internal/app"
+)
+
+type (
+	App       = app.App
+	CustomApp = app.Custom
+)
+
+// New - application basic constructor for
+// e2e tests and external service launch
+func New() (App, error) {
+	return app.New()
+}
