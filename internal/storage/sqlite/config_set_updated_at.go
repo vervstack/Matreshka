d236ab842cdcb7ba51d09fb071731fc3a8499233
AlexSkilled <olegthander@yandex.ru>
2025-05-03 09:11:43 +0300

    [master] saving nodes

--- internal/storage/sqlite/config_set_updated_at.go
+++ internal/storage/sqlite/config_set_updated_at.go
@@ -7,11 +7,12 @@ import (
 	errors "go.redsock.ru/rerrors"
 )
 
-func (p *Provider) SetUpdatedAt(ctx context.Context, serviceName string, time time.Time) error {
+func (p *Provider) SetUpdatedAt(ctx context.Context, serviceName string, updatedAt time.Time) error {
+	updatedAt = updatedAt.In(time.UTC)
 	_, err := p.conn.ExecContext(ctx, `
 		UPDATE configs
 		SET updated_at = $1
-		WHERE name = $2`, time, serviceName)
+		WHERE name = $2`, updatedAt, serviceName)
 	if err != nil {
 		return errors.Wrap(err, "error updating updated_at for config")
 	}
